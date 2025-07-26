b6ef996ba8e14bf7826330116d86a656bb5a2031
Ramon Salva Sanchez <ramonsalvasanchez@gmail.com>
2025-07-26 14:49:07 +0200

    feat: added some extra validations

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,28 +1,44 @@
 const createError = require("http-errors");
 const Comment = require("../models/comments.model");
+const Product = require("../models/products.model");
 
 module.exports.create = (req, res, next) => {
-  Comment.create({
-    ...req.body,
-    product: req.params.id,
-  })
-    .then((address) => {
-      res.status(201).json(address);
+  Product.findById(req.params.id)
+    .then((product) => {
+      if (product) {
+        Comment.create({
+          ...req.body,
+          product: req.params.id,
+        })
+          .then((address) => {
+            res.status(201).json(address);
+          })
+          .catch(next);
+      } else {
+        next(createError(404, "Product not found"));
+      }
     })
     .catch(next);
+  
 };
 
 module.exports.update = (req, res, next) => {
-  Comment.findByIdAndUpdate(req.params.commentId, req.body, {
-    new: true,
-    runValidators: true
-  })
+  Comment.findOne({ product: req.params.id, _id: req.params.commentId })
     .populate("product")
     .then((comment) => {
       if (comment) {
-        res.json(comment);
+        comment.commentary = req.body.commentary;
+        comment.save()
+          .then((comment) => {
+            if (comment) {
+              res.json(comment);
+            } else {
+              next(createError(404, "Comment not found"));
+            }
+          })
+          .catch(next);
       } else {
-        next(createError(404, "Comment not found"));
+        next(createError(404, "Product or commentary id does not match"));
       }
     })
     .catch(next);
